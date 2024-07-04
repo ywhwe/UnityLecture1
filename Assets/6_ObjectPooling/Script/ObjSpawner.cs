a6b32713b385b2177c39fc4dea08ff88e714c347
ywhwe <esromer@kyonggi.ac.kr>
2024-07-04 11:01:36 +0900

    Composed Object Pooling

--- Assets/6_ObjectPooling/Script/ObjSpawner.cs
+++ Assets/6_ObjectPooling/Script/ObjSpawner.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public ObjPool objPool;
+
+    private float degree;
+
+    private WaitForSeconds delay = new WaitForSeconds(0.1f);
+    
     void Start()
     {
-        
+        StartCoroutine(SpawnObj());
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        degree += Time.deltaTime * 720f;
+    }
+
+    private IEnumerator SpawnObj()
+    {
+        while (true)
+        {
+            yield return delay;
+
+            var sphereMover = objPool.GetObj();
+            sphereMover.Init(degree, objPool);
+        }
     }
 }
--- Assets/6_ObjectPooling/Script/SphereMover.cs
+++ Assets/6_ObjectPooling/Script/SphereMover.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SphereMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private ObjPool objPool;
 
-    // Update is called once per frame
+    private WaitForSeconds duration = new WaitForSeconds(3f);
+    
     void Update()
     {
         transform.Translate(0f, 0f, 5 * Time.deltaTime);
+        
+    }
+    
+    public void Init(float angle, ObjPool pool)
+    {
+        transform.position = Vector3.zero;
+        transform.RotateAround(Vector3.zero, Vector3.up, angle);
+
+        objPool = pool;
+        
+        StartCoroutine(Delay());
+    }
+
+    private IEnumerator Delay()
+    {
+        while (true)
+        {
+            yield return duration;
+            
+            objPool.SetObj(this);
+        }
     }
 }
