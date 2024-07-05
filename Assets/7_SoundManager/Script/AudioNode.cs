871a182177daa12a20588277ce1b682aa4f7e749
ywhwe <esromer@kyonggi.ac.kr>
2024-07-05 10:07:24 +0900

    AudioNode added

---
+++ Assets/7_SoundManager/Script/AudioNode.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioNode : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource audioSource;
+
+    public void Play(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+        StartCoroutine(WaitSound());
+    }
+
+    private IEnumerator WaitSound()
+    {
+        yield return new WaitWhile(() => audioSource.isPlaying);
+
+        // yield return new WaitWhile(IsPlaying);
+        
+        SoundManager.instance.SetNode(this);
+    }
+
+    // private bool IsPlaying() => audioSource.isPlaying;
+}
--- Assets/7_SoundManager/Script/SoundManager.cs
+++ Assets/7_SoundManager/Script/SoundManager.cs
@@ -2,25 +2,64 @@ using System;
 using System.Collections;
 using System.Collections.Generic;using OpenCover.Framework.Model;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager instance;
+    
     public AudioData[] soundResources;
     private Dictionary<string, AudioClip> soundDB = new ();
+
+    public int poolSize;
+    public GameObject soundNodePrefab;
+    private Queue<AudioNode> soundPool = new ();
     
     void Awake()
     {
+        instance = this;
+        
         foreach (var soundResource in soundResources)
         {
             soundDB.Add(soundResource.key, soundResource.Clip);
         }
 
         DontDestroyOnLoad(gameObject);
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            MakeNode();
+        }
     }
-    
-    void Update()
+
+    private void MakeNode()
     {
+        var audioNode = Instantiate(soundNodePrefab, transform).GetComponent<AudioNode>();
+        soundPool.Enqueue(audioNode);
+    }
+
+    public void PlaySound(string key, Vector3 pos)
+    {
+        var node = GetNode();
+        node.transform.position = pos;
+        node.Play(soundDB[key]);
+    }
+
+    private AudioNode GetNode()
+    {
+        if (soundPool.Count < 1)
+        {
+            MakeNode();
+        }
+
+        var node = soundPool.Dequeue();
         
+        return node;
+    }
+
+    public void SetNode(AudioNode node)
+    {
+        soundPool.Enqueue(node);
     }
 }
 
